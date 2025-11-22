098408d4425740fadcf4599616e399287e93afc0
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2025-11-22 20:23:00 +0530

    make dashboard controller ui

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -11,7 +11,7 @@ public class Starter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/login_Controller_Form.fxml"))));
         stage.show();
     }
 }
