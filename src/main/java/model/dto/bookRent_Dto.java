321004bc4b18d72e79eade17e22aa2a71e59c1f7
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2025-12-08 14:06:01 +0530

    make packges book rent and make class

--- src/main/java/model/dto/bookRent_Dto.java
+++ src/main/java/model/dto/bookRent_Dto.java
@@ -1,5 +1,20 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class bookRent_Dto {
-    
+    private String rentId;
+    private String customerId;
+    private String bookId;
+    private String rentDate;
+    private String dueDate;
+    private String returnDate;
+    private double bookPrice;
+    private double fine;
+    private String status;
 }
